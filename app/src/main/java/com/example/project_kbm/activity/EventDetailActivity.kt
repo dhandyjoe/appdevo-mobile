b3a6d1de5bf4e6aa4e5518ee478a034af2e2defc
dhandyjoe <dhanjoenkp@gmail.com>
2021-08-13 00:37:28 +0700

    update app

---
+++ app/src/main/java/com/example/project_kbm/activity/EventDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.project_kbm.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_detail)
+    }
+}
