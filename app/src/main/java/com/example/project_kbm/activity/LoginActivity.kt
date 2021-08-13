0889d6fa2d5b244db9961078bd3f1ee8f5b7446f
dhandyjoe <dhanjoenkp@gmail.com>
2021-08-13 10:14:57 +0700

    update

--- app/src/main/java/com/example/project_kbm/activity/LoginActivity.kt
+++ app/src/main/java/com/example/project_kbm/activity/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.example.project_kbm.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.project_kbm.R
+
 import com.example.project_kbm.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private var firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +33,40 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnLogin.setOnClickListener {
+            var status = true
+            if (binding.tvEmail.text.isEmpty()) {
+                binding.tvEmail.error = "Email is required"
+                status = false
+            }
+            if (binding.tvPassword.text.isEmpty()) {
+                binding.tvPassword.error = "Password is required"
+                status = false
+            }
+            if (status) {
+                signIn()
+            }
+        }
+    }
+
+    private fun signIn() {
+        firebaseAuth.signInWithEmailAndPassword(binding.tvEmail.text.toString(), binding.tvPassword.text.toString())
+            .addOnCompleteListener {
+                if(!it.isSuccessful) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Successfully", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed Login: ${it.message}")
+                Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/project_kbm/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/project_kbm/activity/RegisterActivity.kt
@@ -1,12 +1,49 @@
 package com.example.project_kbm.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.project_kbm.R
+import com.example.project_kbm.databinding.ActivityRegisterBinding
+import com.example.project_kbm.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private var firebaseAuth = FirebaseAuth.getInstance()
+    private var firebaseDB = FirebaseFirestore.getInstance()
+    val DATA_USERS = "Users"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun register() {
+        firebaseAuth.createUserWithEmailAndPassword(binding.tvEmail.text.toString(), binding.tvPassword.text.toString())
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    startActivity(Intent(this, RegisterActivity::class.java))
+                } else if (firebaseAuth.uid != null) {
+                    val name = binding.tvName.text.toString()
+                    val email = binding.tvEmail.text.toString()
+                    val user = User(name, email)
+                    firebaseDB.collection(DATA_USERS).document(firebaseAuth.uid!!).set(user)
+
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed Login: ${it.message}")
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
     }
 }
